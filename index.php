cbdaca1b4afaee995ea0f154e6d11b80246383ea
Tomas Lagren <tomas@lagren.com>
2016-11-11 22:15:06 +0100

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Simple PHP test</title>
+</head>
+<body>
+<h1>Simple PHP test</h1>
+
+<?php
+
+echo "<p>Running on server with IP address <strong>" . $_SERVER['SERVER_ADDR'] . "</strong></p>\n";
+
+?>
+
+</body>
+</html>
+
